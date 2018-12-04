2f8baf104c13a2f573d9b2083b428888f60d868e
Blake Fasse <blakefasse@gmail.com>
2018-12-04 13:20:52 -0600

    Doing refresher on Kotlin

---
+++ Kotlin/variables.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+    
+    val name = "Blake"
+    // val name: String = "Blake"
+    var isAwesome = true
+    isAwesome = false
+
+    println("Is " + name + " awesome? The answer is " + isAwesome)
+
+    var a = 3
+    var b = 6 
+    println(a + b)
+
+    val doubleNum = 123.45 // 64-bit 
+    val floatNum = 123.45f // 32-bit 
+    val longNum = 123.45L // 64-bit
+
+    val aDouble = a.toDouble()
+
+    var hero: String
+    hero = "Batman"
+    println(hero)
+}
